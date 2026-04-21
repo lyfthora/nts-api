8c5733b20679f721d6202e0a8ef1cee3245ca612
brayan <marcosbrayan03@gmail.com>
2026-04-21 20:27:11 +0200

    feat(auth): add user authentication with JWT and bcrypt

---
+++ prisma/migrations/20260421175149_add_user_auth/migration.sql
@@ -0,0 +1,25 @@
+-- AlterTable
+ALTER TABLE "Folder" ADD COLUMN     "userId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "Note" ADD COLUMN     "userId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
